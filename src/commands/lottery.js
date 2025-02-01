fdc67f0f44bd51c6c795ab6b506e1f5648c9a831
Vanxh <hello@vanxh.dev>
2025-02-02 01:38:25 +0400

    fix lottery

--- src/commands/lottery.js
+++ src/commands/lottery.js
@@ -329,7 +329,7 @@ export class LotteryCommand extends Command {
                     .fetch(GAMBLING_CHANNEL_ID)
                     .then((channel) => {
                         channel.send(
-                            "❌ **BETTER LUCK NEXT TIME** 🚫\nUnfortunately, no one won this time. @everyone"
+                            "❌ **BETTER LUCK NEXT TIME** ❌\nUnfortunately, no one won this time. @everyone"
                         );
                     });
 
